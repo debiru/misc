77bf844257377818abcc507eeb25661809797651
coeurl <coeuerl@lavoscore.org>
2022-02-04 03:50:13 +0900

    OCSP checker: supported json

--- web/code/snippet/check_ocsp_response.php
+++ web/code/snippet/check_ocsp_response.php
@@ -23,6 +23,10 @@ class OCSP {
   const CA_CERT = '/var/www/hosts/web/ssl/chain.pem';
   const RESPONDER = 'r3.o.lencr.org';
 
+  public static function isJsonResponse() {
+    return @$_GET['type'] === 'json';
+  }
+
   public static function mycmd() {
     $arg_list = func_get_args();
     $format = array_shift($arg_list);
@@ -40,9 +44,11 @@ class OCSP {
   public static function apiGetOCSP($serial) {
     $url = sprintf('http://%s', self::RESPONDER);
     $host = sprintf('HOST=%s', self::RESPONDER);
-    $cmd = self::mycmd("openssl ocsp -noverify -no_nonce -issuer %s -serial %s -url %s -header %s -text", self::CA_CERT, $serial, $url, $host);
+    $grep = self::isJsonResponse() ? ' | ag "^(?:      Serial Number|    This Update|    Next Update)" | perl -pe "s/^\s*[^:]+:\s*//"' : '';
+    $format = sprintf("openssl ocsp -noverify -no_nonce -issuer %%s -serial %%s -url %%s -header %%s -text%s", $grep);
+    $cmd = self::mycmd($format, self::CA_CERT, $serial, $url, $host);
     self::myexec($cmd, $output);
-    return implode(PHP_EOL, $output);
+    return $output;
   }
 
   public static function apiGetSerial($domain) {
@@ -63,35 +69,54 @@ class OCSP {
     return $serial;
   }
 
-  public static function failed($query) {
-    if (empty($query)) {
-      echo "Let's Encrypt: OCSP checker\n\n";
-      echo "Usage:\n";
-      echo "  https://ssl.lavoscore.org/api/sslcert-expires/ocsp/?q=\n\n";
-      echo "       Serial: ?q=04460bAE02A944E3D3FE00E759F4C8FFF007\n";
-      echo "   Serial Hex: ?q=0x04460bAE02A944E3D3FE00E759F4C8FFF007\n";
-      echo "  Domain Name: ?q=lavoscore.org\n";
+  public static function requestOCSP($query) {
+    $serial = self::getSerial($query);
+
+    $result = empty($serial) ? null : self::apiGetOCSP($serial);
+
+    if (self::isJsonResponse()) {
+
+      self::outputJson($result, $query);
     }
     else {
-      echo "Failed. It appears to be an invalid value. Require \"q\" parameter as Serial or DomainName.\n\n";
-      var_dump($query);
+      self::outputText($result, $query);
     }
   }
 
-  public static function requestOCSP($query) {
+  public static function outputJson($result, $query) {
+    header('Content-Type: application/json; charset=utf-8');
+
+    $ocsp = [
+      'serial' => $result[0] ?? null,
+      'this_update' => $result[1] ?? null,
+      'next_update' => $result[2] ?? null,
+    ];
+
+    $json = json_encode($ocsp, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    echo $json, PHP_EOL;
+  }
+
+  public static function outputText($result, $query) {
     header('Content-Type: text/plain; charset=utf-8');
-    $serial = self::getSerial($query);
-    if (empty($serial)) {
-      self::failed($query);
-    }
-    else {
-      $result = self::apiGetOCSP($serial);
-      if (!empty($result)) {
-        echo $result;
+
+    if (empty($result)) {
+      if (empty($query)) {
+        echo "Let's Encrypt: OCSP checker\n\n";
+        echo "Usage:\n";
+        echo "  https://ssl.lavoscore.org/api/sslcert-expires/ocsp/?q=\n";
+        echo "  https://ssl.lavoscore.org/api/sslcert-expires/ocsp/?type=json&q=\n";
+        echo "\n";
+        echo "       Serial: ?q=04460bAE02A944E3D3FE00E759F4C8FFF007\n";
+        echo "   Serial Hex: ?q=0x04460bAE02A944E3D3FE00E759F4C8FFF007\n";
+        echo "  Domain Name: ?q=lavoscore.org\n";
       }
       else {
-        self::failed($query);
+        echo "Failed. It appears to be an invalid value. Require \"q\" parameter as Serial or DomainName.\n\n";
+        var_dump($query);
       }
+    }
+    else {
+      echo implode(PHP_EOL, $result), PHP_EOL;
     }
   }
 }
